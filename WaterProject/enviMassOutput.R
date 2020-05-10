7da4003451d18b6a6331d3966192b6bc52509655
trainorp <patrick.trainor@louisville.edu>
2020-05-09 23:12:57 -0600

    working

--- WaterProject/enviMassOutput.R
+++ WaterProject/enviMassOutput.R
@@ -2,9 +2,43 @@ library(enviMass)
 setwd("C:/Users/ptrainor/gdrive/CAIL/WaterProject")
 
 # Import profile data from enviMass results:
-load_profileList("C:/Users/ptrainor/gdrive/CAIL/WaterProject/WaterProjectDemo2/results/profileList_pos")
-profileList_poss
+load_profileList("./WaterProjectDemo2/results/profileList_pos")
+profileList_pos
 
 profs <- enviMass::profiles_to_matrix(profileList_pos)
 peaks <- profileList_pos$peaks
 # peaks3753 <- peaks[peaks[,"profileIDs"] == 3753, ]
+
+# Internal standards:
+load("./WaterProjectDemo2/results/screening/res_IS_pos_screen")
+load("./WaterProjectDemo2/results/screening/results_screen_IS_pos")
+
+
+results_screen_IS_pos[[6]]
+save.image(file = "enviMassOutput_20200509.RData")
+
+res_IS_pos_screen[[1]]$`1`
+res_IS_pos_screen[[1]]$`2`
+res_IS_pos_screen[[1]]$`3`
+x <- res_IS_pos_screen[[1]]$`8`
+
+getISTD <- function(x){
+  x <- x[[1]]
+  lNames <- names(x)
+  tDF1 <- matrix(nrow = length(x$`m/z`), ncol = length(lNames))
+  tDF1 <- as.data.frame(tDF1)
+  names(tDF1) <- lNames
+  tDF1$Peaks <- x$Peaks[,2]
+  tDF1$score_1 <- x$score_1
+  tDF1$score_2 <- x$score_2
+  tDF1$`ppm deviation` <- x$`ppm deviation`
+  tDF1$`RT deviation from mean` <- x$`RT deviation from mean`
+  tDF1$`rescale factor` <- x$`rescale factor`
+  tDF1$`m/z` <- x$`m/z`
+  tDF1$Intensity <- x$Intensity
+  tDF1$RT <- x$RT
+  tDF1$file_ID <- x$file_ID
+  return(tDF1)
+}
+getISTD(res_IS_pos_screen[[1]]$`3`)
+lapply(res_IS_pos_screen[[1]], getISTD)
--- WaterProject/statisticalAnalysis.R
+++ WaterProject/statisticalAnalysis.R
@@ -1,2 +1,51 @@
-setwd("C:/Users/ptrainor/gdrive/CAIL/WaterProject")
+############ Prereqs ############
+options(stringsAsFactors = FALSE, scipen = 600)
+oldPar <- par()
+
+library(tidyverse)
+
+os <- Sys.info()["sysname"]
+baseDir <- ifelse(os == "Windows", "C:/Users/ptrainor/gdrive/", "~/gdrive/")
+setwd(paste0(baseDir, "CAIL/WaterProject"))
+
+# Read in sample annotation:
 sampleAnno <- read.csv("SampleAnno_20200509.csv")
+
+# Load profile data:
+load("enviMassOutput_20200509.RData")
+
+# Set rownames on that data:
+rownames(profs) <- sampleAnno$Name[match(rownames(profs), sampleAnno$ID)]
+
+# Which are 0:
+which(profs == 0, arr.ind = TRUE)
+
+# Imputation for missing values:
+impFun <- function(x){
+  x[x == 0] <- min(x[x > 0]) / 2
+  return(x)
+}
+profs <- apply(profs, 2, impFun)
+
+# Convert to log-scale:
+profs <- log2(profs)
+profs <- scale(profs, center = TRUE, scale = FALSE)
+
+# Entropy filter:
+entropyFun <- function(x) entropy::entropy(entropy::discretize(x, 5), unit = "log2")
+colEntropies <- apply(profs, 2, entropyFun)
+hist(colEntropies)
+profs <- profs[, colEntropies > 1]
+
+heatmap(profs)
+
+pca1 <- prcomp(profs)
+plot(pca1)
+pca1DF <- as.data.frame(pca1$x[, 1:2])
+pca1DF$sampleID <- rownames(pca1DF)
+pca1DF$sampleID <- gsub("90percent_", "", gsub("90per_", "", pca1DF$sampleID))
+set.seed(3)
+ggplot(pca1DF, aes(x = PC1, y = PC2, label = sampleID)) + geom_point() + geom_text_repel() + 
+  theme_bw() + labs(title = "PCA prior to normalization")
+
+########### IS-based normalization ###########

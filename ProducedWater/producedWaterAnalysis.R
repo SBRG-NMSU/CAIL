09d1a0fa60a7254ddc10bb21f4f639abf6b8f3f4
trainorp <patrick.trainor@louisville.edu>
2020-07-21 08:35:45 -0600

    New produced water work

---
+++ ProducedWater/producedWaterAnalysis.R
@@ -0,0 +1,11 @@
+############ Prereqs ############
+# Begin always run
+options(stringsAsFactors = FALSE, scipen = 600, max.print = 100000)
+oldPar <- par()
+
+library(tidyverse)
+
+os <- Sys.info()["sysname"]
+baseDir <- ifelse(os == "Windows", "C:/Users/ptrainor/gdrive/", "~/gdrive/")
+setwd(paste0(baseDir, "CAIL/ProducedWater"))
+# End always run

9b6031ed15cd758453f430800121c27fc7579f3e
Patrick Trainor <ptrainor@nmsu.edu>
2020-07-24 18:32:13 -0600

    Update producedWaterAnalysis.R

--- ProducedWater/producedWaterAnalysis.R
+++ ProducedWater/producedWaterAnalysis.R
@@ -11,6 +11,39 @@ baseDir <- "~/GitHub/cail/"
 setwd(paste0(baseDir, "ProducedWater"))
 # End always run
 
+############ Import enviMass profile data ############
+load("enviMassOutput_20200721.RData")
+profs0 <- profs
+profs <- read.csv("profiled_peaks.csv")
+
+profInfo2 <- profs[, !grepl("\\.sample\\.", names(profs))]
+profs <- profs[, grepl("\\.sample\\.", names(profs))]
+
+names(profs) <- gsub("\\.", "", gsub("X\\d\\.sample\\.", "", names(profs)))
+profs <- t(profs)
+colnames(profs) <- profInfo2$profile_ID
+
+# Internal standards:
+iSTDs <- readxl::read_excel("iSTDs.xlsx")
+
+# Process profile annotation data:
+profInfo <- as.data.frame(profInfo)
+profInfo$profile_ID <- as.character(as.integer(profInfo$profile_ID))
+
+############ Some processing of iSTD data ############
+# Split ID from the enviMass output:
+iSTDDF$iSTD_ID <- gsub("_none_none_none", "", iSTDDF$iSTD_ID)
+temp1 <- str_split(iSTDDF$iSTD_ID, "_", simplify = TRUE)
+iSTDDF$iSTD_ID <- temp1[,1]
+iSTDDF$adduct <- temp1[,2]
+
+# Join our db file:
+iSTDs$ID <- as.character(iSTDs$ID)
+iSTDDF <- iSTDDF %>% left_join(iSTDs, by = c("iSTD_ID" = "ID"))
+iSTDDF$iso <- ifelse(abs(iSTDDF$`m/z` - iSTDDF$Da) < .050, "0", "")
+mISTD <- iSTDDF %>% filter(adduct == "M+H" & iso == "0")
+presentISTD <- mISTD %>% group_by(file_ID, Name) %>% summarize(n = n()) %>% spread(key = "Name", value = "n")
+
 ############ Import MS/MS data ############
 # Import: 
 msmsData1 <- read.delim("MsmsIncluded_0_2020721724.txt", skip = 4, header = TRUE)
@@ -28,6 +61,9 @@ msmsData1$neutralMass[msmsData1$Adducttype == "[M+Na]+"] <-
 
 msmsData1$neutralMass[msmsData1$Adducttype == "[M+NH4]+"] <- 
   msmsData1$AverageMz[msmsData1$Adducttype == "[M+NH4]+"] - 18.033823
+
+msmsData1$neutralMass[msmsData1$Adducttype == "[M+K]+"] <- 
+  msmsData1$AverageMz[msmsData1$Adducttype == "[M+K]+"] - 38.963158
 
 # Functions:
 getMSMS <- function(x){
@@ -51,12 +87,9 @@ makeList <- function(x){
 # Call functions:
 msmsData2 <- apply(msmsData1, 1, function(x) makeList(x))
 
-############ Import Molecular Formula Enumeration ############
-
-
 ############ Match MS1 ############
-rtTol <- 20 / 60
-mzTol <- 3
+rtTol <- 5 / 60
+mzTol <- 2
 
 # Add tolerances to MS1 data of MS/MS data:
 msmsData1$rtLower <- msmsData1$AverageRtmin - rtTol
@@ -64,7 +97,23 @@ msmsData1$rtUpper <- msmsData1$AverageRtmin + rtTol
 msmsData1$mzLower <- msmsData1$AverageMz - msmsData1$AverageMz * mzTol * 1e-6
 msmsData1$mzUpper <- msmsData1$AverageMz + msmsData1$AverageMz * mzTol * 1e-6
 
-
+# Join here:
+profInfo2$msmsMatch <- profInfo2$msmsMatchU <- ""
+for(i in 1:nrow(profInfo2)){
+  match1 <- profInfo2$profile_mean_mass[i] > msmsData1$mzLower & profInfo2$profile_mean_mass[i] < msmsData1$mzUpper
+  match2 <- profInfo2$profile_mean_RT_s[i] / 60 > msmsData1$rtLower & profInfo2$profile_mean_RT_s[i] / 60 < msmsData1$rtUpper
+  match3 <- msmsData1$AlignmentID[which(match1 & match2)]
+  
+  profInfo2$msmsMatch[i] <- paste(match3, collapse = ";")
+  
+  if(length(match3) > 1){
+    largeSN <- match3[which.max(msmsData1$SNaverage[msmsData1$AlignmentID %in% match3])]
+    profInfo2$msmsMatchU[i] <- largeSN
+  }
+  else{
+    profInfo2$msmsMatchU[i] <- profInfo2$msmsMatch[i]
+  }
+}
 
 ############ MetFragR queries ############
 for(i in 1:100){

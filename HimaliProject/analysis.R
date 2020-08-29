db2ec534fbf442a4c26935175cf6b342a583a727
Patrick Trainor <ptrainor@nmsu.edu>
2020-08-29 15:32:22 -0600

    Update analysis.R

--- HimaliProject/analysis.R
+++ HimaliProject/analysis.R
@@ -41,33 +41,30 @@ files1 <- list.files("C:/Users/ptrainor/Dropbox (NMSU Advanced)/Patrick/Himali/m
 df1 <- MSnbase::readMSData(files1, mode = "onDisk")
 df2 <-  MSnbase::filterMsLevel(df1, 2)
 df1Header <- MSnbase::header(df1)
+df1Header$sName <- rownames(df1Header)
 
 ########### Process MS/MS data ###########
-df2Anno <- data.frame(mz = MSnbase::precursorMz(df2), precursorScan = MSnbase::precScanNum(df2))
+df2Anno <- data.frame(precursorMz = MSnbase::precursorMz(df2), precursorScan = MSnbase::precScanNum(df2))
+df2Anno$sName <- rownames(df2Anno)
+df2Anno <- df2Anno %>% left_join(df1Header %>% select(sName, rt = retentionTime))
 
-rtTol <- 10 / 60
+rtTol <- 10
 mzTol <- 2
 
-# Add tolerances to MS1 data of MS/MS data:
-msmsData1$rtLower <- msmsData1$AverageRtmin - rtTol
-msmsData1$rtUpper <- msmsData1$AverageRtmin + rtTol
-msmsData1$mzLower <- msmsData1$AverageMz - msmsData1$AverageMz * mzTol * 1e-6
-msmsData1$mzUpper <- msmsData1$AverageMz + msmsData1$AverageMz * mzTol * 1e-6
+# Add tolerances to MS/MS data anno:
+df2Anno$rtLower <- df2Anno$rt - rtTol
+df2Anno$rtUpper <- df2Anno$rt + rtTol
+df2Anno$mzLower <- df2Anno$precursorMz - df2Anno$precursorMz * mzTol * 1e-6
+df2Anno$mzUpper <- df2Anno$precursorMz + df2Anno$precursorMz * mzTol * 1e-6
 
 # Join here:
 profInfo2$msmsMatch <- profInfo2$msmsMatchU <- ""
 for(i in 1:nrow(profInfo2)){
-  match1 <- profInfo2$profile_mean_mass[i] > msmsData1$mzLower & profInfo2$profile_mean_mass[i] < msmsData1$mzUpper
-  match2 <- profInfo2$profile_mean_RT_s[i] / 60 > msmsData1$rtLower & profInfo2$profile_mean_RT_s[i] / 60 < msmsData1$rtUpper
-  match3 <- msmsData1$AlignmentID[which(match1 & match2)]
+  match1 <- profInfo2$profile_mean_mass[i] > df2Anno$mzLower & profInfo2$profile_mean_mass[i] < df2Anno$mzUpper
+  match2 <- profInfo2$profile_mean_RT_s[i] > df2Anno$rtLower & profInfo2$profile_mean_RT_s[i] < df2Anno$rtUpper
+  match3 <- df2Anno$sName[which(match1 & match2)]
   
+  msmsMatch3 <- df2[rownames(df2@featureData) %in% match3]
+  MSnbase::combineSpectra(msmsMatch3, method = consensusSpectrum())
   profInfo2$msmsMatch[i] <- paste(match3, collapse = ";")
-  
-  if(length(match3) > 1){
-    largeSN <- match3[which.max(msmsData1$SNaverage[msmsData1$AlignmentID %in% match3])]
-    profInfo2$msmsMatchU[i] <- largeSN
-  }
-  else{
-    profInfo2$msmsMatchU[i] <- profInfo2$msmsMatch[i]
-  }
 }
